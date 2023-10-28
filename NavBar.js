b4b8463fb7a7e00970fcf57bd82b1234531921b4
Jamie Alizadeh <JamieAlizadeh@gmail.com>
2023-10-28 15:16:39 -0500

    Add files via upload

--- NavBar.js
+++ NavBar.js
@@ -8,12 +8,12 @@ document.write("");
 document.write("        <!-- Navigation -->");
 document.write("        <nav class=\"nav-menu\" role=\"navigation\">");
 document.write("          <ul class=\"nav-links\">");
-document.write("            <li><a href=\"index.html\" title=\"Home\">Home<\/a><\/li>");
-document.write("            <li><a href=\"article.html\" title=\"World\">World<\/a>");
-document.write("            <li><a href=\"politics.html\" title=\"Politics\">Politics<\/a><\/li>");
-document.write("            <li><a href=\"business.html\" title=\"Business\">Business<\/a><\/li>");
-document.write("            <li><a href=\"sport.html\" title=\"Sport\">Sport<\/a><\/li>");
-document.write("            <li><a href=\"entertainment.html\" title=\"Entertainment\">Entertainment<\/a><\/li>");
+document.write("            <li><a href=\"news/index.html\" title=\"Home\">Home<\/a><\/li>");
+document.write("            <li><a href=\"news/article.html\" title=\"World\">World<\/a>");
+document.write("            <li><a href=\"news/politics.html\" title=\"Politics\">Politics<\/a><\/li>");
+document.write("            <li><a href=\"news/business.html\" title=\"Business\">Business<\/a><\/li>");
+document.write("            <li><a href=\"news/sport.html\" title=\"Sport\">Sport<\/a><\/li>");
+document.write("            <li><a href=\"news/entertainment.html\" title=\"Entertainment\">Entertainment<\/a><\/li>");
 document.write("            <li>");
 document.write("              <a href=\"#more\" title=\"More\">More...<span class=\"dropdown-toggle\" aria-expanded=\"false\"><\/span>");
 document.write("                <span class=\"screen-reader-text\">Expand child menu<\/span><\/a>");
@@ -35,3 +35,45 @@ document.write("          <\/div>");
 document.write("        <\/nav>");
 document.write("      <\/section> <!-- end of class=\"nav-section\"-->");
 document.write("");
+
+/*
+
+<section class="nav-section">
+
+        <!-- Logo -->
+        <div class="logo">
+          <a href="index.html" title="Frivizn logo">The Canadian Populist</a>
+        </div>
+
+        <!-- Navigation -->
+        <nav class="nav-menu" role="navigation">
+          <ul class="nav-links">
+            <li><a href="index.html" title="Home">Home</a></li>
+            <li><a href="article.html" title="World">World</a>
+            <li><a href="politics.html" title="Politics">Politics</a></li>
+            <li><a href="business.html" title="Business">Business</a></li>
+            <li><a href="sport.html" title="Sport">Sport</a></li>
+            <li><a href="entertainment.html" title="Entertainment">Entertainment</a></li>
+            <li>
+              <a href="#more" title="More">More...<span class="dropdown-toggle" aria-expanded="false"></span>
+                <span class="screen-reader-text">Expand child menu</span></a>
+              <ul class="sub-menu">
+                <li><a href="#" title="Science">Science</a></li>
+                <li><a href="#" title="Travel">Travel</a></li>
+                <li><a href="#" title="Health">Health</a></li>
+                <li><a href="#" title="Style">Style</a></li>
+                <li><a href="#" title="Weather">Weather</a></li>
+                <li><a href="#" title="Video">Video</a></li>
+              </ul>
+            </li>
+          </ul>
+          <div class="burger">
+            <div class="line-1"></div>
+            <div class="line-2"></div>
+            <div class="line-3"></div>
+          </div>
+        </nav>
+      </section> <!-- end of class="nav-section"-->
+
+      */
+
