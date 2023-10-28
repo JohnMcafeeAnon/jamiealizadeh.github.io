cb0a3916d6e6133b956ea109d1fef483cfb3315c
Jamie Alizadeh <JamieAlizadeh@gmail.com>
2023-10-28 13:18:02 -0500

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,104 @@
+document.write("");
+document.write("    <!-- Footer Links -->");
+document.write("    <div class=\"footer-links\">");
+document.write("");
+document.write("      <ul class=\"social-media-links\">");
+document.write("        <li><a href=\"http:\/\/twitter.com\"><span class=\"screen-reader-text\">Twitter<\/span><\/a><\/li>");
+document.write("        <li><a href=\"http:\/\/facebook.com\"><span class=\"screen-reader-text\">Facebook<\/span><\/a><\/li>");
+document.write("        <li><a href=\"http:\/\/linkedin.com\"><span class=\"screen-reader-text\">LinkedIn<\/span><\/a><\/li>");
+document.write("        <li><a href=\"http:\/\/youtube.com\"><span class=\"screen-reader-text\">YouTube<\/span><\/a><\/li>");
+document.write("        <li><a href=\"http:\/\/instagram.com\"><span class=\"screen-reader-text\">Instagram<\/span><\/a><\/li>");
+document.write("      <\/ul>");
+document.write("");
+document.write("      <ul class=\"main-footer-links\">");
+document.write("        <li><a href=\"#world\" title=\"World\">World<\/a>");
+document.write("        <li><a href=\"#politics\" title=\"Politics\">Politics<\/a><\/li>");
+document.write("        <li><a href=\"#business\" title=\"Business\">Business<\/a><\/li>");
+document.write("        <li><a href=\"#sport\" title=\"Sport\">Sport<\/a><\/li>");
+document.write("        <li><a href=\"#entertainment\" title=\"Entertainment\">Entertainment<\/a><\/li>");
+document.write("        <li><a href=\"#science\" title=\"Science\">Science<\/a><\/li>");
+document.write("        <li><a href=\"#travel\" title=\"Travel\">Travel<\/a><\/li>");
+document.write("        <li><a href=\"#health\" title=\"Health\">Health<\/a><\/li>");
+document.write("        <li><a href=\"#style\" title=\"Style\">Style<\/a><\/li>");
+document.write("        <li><a href=\"#weather\" title=\"Weather\">Weather<\/a><\/li>");
+document.write("        <li><a href=\"#video\" title=\"Video\">Video<\/a><\/li>");
+document.write("      <\/ul>");
+document.write("");
+document.write("      <ul class=\"info-links\">");
+document.write("        <li><a href=\"#terms-of-use\" title=\"Terms of Use\">Terms of Use<\/a><\/li>");
+document.write("        <li><a href=\"#privacy-policy\" title=\"Privacy Policy\">Privacy Policy<\/a><\/li>");
+document.write("        <li><a href=\"#about-us\" title=\"About Us\">About Us<\/a><\/li>");
+document.write("        <li><a href=\"#advertising\" title=\"Advertising\">Advertising<\/a><\/li>");
+document.write("        <li><a href=\"#sitemap\" title=\"Sitemap\">Sitemap<\/a><\/li>");
+document.write("      <\/ul>");
+document.write("");
+document.write("      <ul class=\"our-address\">");
+document.write("        <li>Our Address:<\/li>");
+document.write("        <li>Main Street 123<\/li>");
+document.write("        <li>New York, NY 10000<\/li>");
+document.write("        <li>111-222-3333<\/li>");
+document.write("      <\/ul>");
+document.write("");
+document.write("    <\/div> <!-- End of class=\"footer-links\" -->");
+document.write("");
+document.write("    <!-- Footer Credit -->");
+document.write("    <footer role=\"contentinfo\">");
+document.write("      <p>2019 &copy; The Canadian Populist<\/p>");
+document.write("      <a class=\"scroll-top\" href=\"#top\"><\/a>");
+document.write("    <\/footer>");
+document.write("");
+document.write("  <\/div> <!-- end of class=\"content-wrap\" -->");
+
+/*
+
+    <!-- Footer Links -->
+    <div class="footer-links">
+
+      <ul class="social-media-links">
+        <li><a href="http://twitter.com"><span class="screen-reader-text">Twitter</span></a></li>
+        <li><a href="http://facebook.com"><span class="screen-reader-text">Facebook</span></a></li>
+        <li><a href="http://linkedin.com"><span class="screen-reader-text">LinkedIn</span></a></li>
+        <li><a href="http://youtube.com"><span class="screen-reader-text">YouTube</span></a></li>
+        <li><a href="http://instagram.com"><span class="screen-reader-text">Instagram</span></a></li>
+      </ul>
+
+      <ul class="main-footer-links">
+        <li><a href="#world" title="World">World</a>
+        <li><a href="#politics" title="Politics">Politics</a></li>
+        <li><a href="#business" title="Business">Business</a></li>
+        <li><a href="#sport" title="Sport">Sport</a></li>
+        <li><a href="#entertainment" title="Entertainment">Entertainment</a></li>
+        <li><a href="#science" title="Science">Science</a></li>
+        <li><a href="#travel" title="Travel">Travel</a></li>
+        <li><a href="#health" title="Health">Health</a></li>
+        <li><a href="#style" title="Style">Style</a></li>
+        <li><a href="#weather" title="Weather">Weather</a></li>
+        <li><a href="#video" title="Video">Video</a></li>
+      </ul>
+
+      <ul class="info-links">
+        <li><a href="#terms-of-use" title="Terms of Use">Terms of Use</a></li>
+        <li><a href="#privacy-policy" title="Privacy Policy">Privacy Policy</a></li>
+        <li><a href="#about-us" title="About Us">About Us</a></li>
+        <li><a href="#advertising" title="Advertising">Advertising</a></li>
+        <li><a href="#sitemap" title="Sitemap">Sitemap</a></li>
+      </ul>
+
+      <ul class="our-address">
+        <li>Our Address:</li>
+        <li>Main Street 123</li>
+        <li>New York, NY 10000</li>
+        <li>111-222-3333</li>
+      </ul>
+
+    </div> <!-- End of class="footer-links" -->
+
+    <!-- Footer Credit -->
+    <footer role="contentinfo">
+      <p>2019 &copy; The Canadian Populist</p>
+      <a class="scroll-top" href="#top"></a>
+    </footer>
+
+  </div> <!-- end of class="content-wrap" -->
+
+*/
